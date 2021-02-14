7140a23c582f26b03d7bf352f3fcffc3f379ef9c
christine <christine.zerrouk@gmail.com>
2021-02-14 17:56:32 +0100

    modifications mineures sur le code
    Signed-off-by: christine <christine.zerrouk@gmail.com>

--- src/main/java/com/parkit/parkingsystem/model/Ticket.java
+++ src/main/java/com/parkit/parkingsystem/model/Ticket.java
@@ -1,61 +1,71 @@
 package com.parkit.parkingsystem.model;
 
-import java.util.Calendar;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Date;
 
 public class Ticket {
-    private int id;
+    private Integer id;
     private ParkingSpot parkingSpot;
     private String vehicleRegNumber;
     private double price;
     private Date inTime;
     private Date outTime;
 
+
     public int getId() {
         return id;
+
     }
 
-    public void setId(int id) {
+    public Ticket setId(int id) {
         this.id = id;
+        return this;
     }
 
     public ParkingSpot getParkingSpot() {
         return parkingSpot;
     }
 
-    public void setParkingSpot(ParkingSpot parkingSpot) {
+    public Ticket setParkingSpot(ParkingSpot parkingSpot) {
         this.parkingSpot = parkingSpot;
+        return this;
     }
 
     public String getVehicleRegNumber() {
         return vehicleRegNumber;
     }
 
-    public void setVehicleRegNumber(String vehicleRegNumber) {
+    public Ticket setVehicleRegNumber(String vehicleRegNumber) {
         this.vehicleRegNumber = vehicleRegNumber;
+        return this;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    public Ticket setPrice(double price) {
         this.price = price;
+        return this;
     }
 
     public Date getInTime() {
         return inTime;
     }
 
-    public void setInTime(Date inTime) {
+    public Ticket setInTime(Date inTime) {
         this.inTime = inTime;
+        return this;
     }
 
     public Date getOutTime() {
         return outTime;
     }
 
-    public void setOutTime(Date outTime) {
+    public Ticket setOutTime(Date outTime) {
         this.outTime = outTime;
+        return this;
     }
 }
