bf3a542bc8bc3082d2227985a3db193230d607fc
christine <christine.zerrouk@gmail.com>
2021-02-14 17:57:02 +0100

    classe exception
    Signed-off-by: christine <christine.zerrouk@gmail.com>

---
+++ src/test/java/com/parkit/parkingsystem/exception/TicketProcessingException.java
@@ -0,0 +1,7 @@
+package com.parkit.parkingsystem.exception;
+
+public class TicketProcessingException extends RuntimeException {
+    public TicketProcessingException(String message) {
+        super(message);
+    }
+}
